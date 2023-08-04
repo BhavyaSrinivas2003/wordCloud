24e45268977e75e4301144feef46cf008ac96ed3
Bhavya Srinivas <127350976+BhavyaSrinivas2003@users.noreply.github.com>
2023-08-04 18:15:04 +0530

    Add files via upload

---
+++ wordCloud.js
@@ -0,0 +1,30 @@
+function cloudFun() {
+    let wordCloud = ["Hello", "hii", "how", "what", "you", "yourself", "name", "victory", "food", "lovely", "beautiful", "written", "where", "who", "awesome"];
+    let wordCloudLength = wordCloud.length;
+    let wordsContainer = document.getElementById("wordsContainer");
+    let paraElement = document.getElementById("errorMsg");
+    let count = 0;
+    for (let item of wordCloud) {
+        let spanElement = document.createElement("span");
+        spanElement.style.fontSize = Math.floor(Math.random() * 40) + "px";
+        spanElement.textContent = item;
+        spanElement.classList.add("m-3");
+        wordsContainer.appendChild(spanElement);
+    }
+
+    function addBtn() {
+        let spanElement = document.createElement("span");
+        let inputElement = document.getElementById("userInput");
+        let inputElementValue = inputElement.value;
+        spanElement.classList.add("m-3");
+        spanElement.style.fontSize = Math.floor(Math.random() * 40) + "px";
+        if (inputElementValue !== "") {
+            spanElement.textContent = inputElementValue;
+            wordsContainer.appendChild(spanElement);
+            inputElement.value = "";
+        } else {
+            paraElement.textContent = "Please enter a word";
+        }
+
+    }
+}
